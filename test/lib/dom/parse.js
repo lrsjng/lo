05095383def6dc1045f5474cca8379cdf8093955
Lars Jung <lrsjng@gmail.com>
2016-08-12 18:01:17 +0200

    Minor.

--- test/lib/dom/parse.js
+++ test/lib/dom/parse.js
@@ -2,96 +2,7 @@ const {test, assert, insp} = require('scar');
 const {lo} = require('../../util');
 
 test('lo.dom() - create element', () => {
-    [
-        'a',
-        'address',
-        'area',
-        'article',
-        'audio',
-        'b',
-        'button',
-        'canvas',
-        'caption',
-        'code',
-        'col',
-        'colgroup',
-        'content',
-        'datalist',
-        'dd',
-        'decorator',
-        'del',
-        'details',
-        'dialog',
-        'div',
-        'dl',
-        'dt',
-        'element',
-        'em',
-        'embed',
-        'fieldset',
-        'figcaption',
-        'figure',
-        'font',
-        'footer',
-        'form',
-        'h1',
-        'h2',
-        'h3',
-        'h4',
-        'h5',
-        'h6',
-        'header',
-        'hgroup',
-        'hr',
-        'i',
-        'iframe',
-        'img',
-        'input',
-        'ins',
-        'label',
-        'legend',
-        'li',
-        'link',
-        'main',
-        'map',
-        'menu',
-        'menuitem',
-        'meta',
-        'meter',
-        'nav',
-        'noscript',
-        'object',
-        'ol',
-        'optgroup',
-        'option',
-        'output',
-        'p',
-        'param',
-        'pre',
-        'progress',
-        'script',
-        'section',
-        'select',
-        'shadow',
-        'source',
-        'span',
-        'strong',
-        'style',
-        'summary',
-        'table',
-        'tbody',
-        'td',
-        'template',
-        'textarea',
-        'tfoot',
-        'th',
-        'thead',
-        'title',
-        'tr',
-        'track',
-        'ul',
-        'video'
-    ].forEach((name, idx) => {
+    'a address area article audio b button canvas caption code col colgroup content datalist dd decorator del details dialog div dl dt element em embed fieldset figcaption figure font footer form h1 h2 h3 h4 h5 h6 header hgroup hr i iframe img input ins label legend li link main map menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress script section select shadow source span strong style summary table tbody td template textarea tfoot th thead title tr track ul video'.split(/\s+/).forEach((name, idx) => {
         const msg = `fix#${idx}: ${insp(name)}`;
         const tag = `<${name}>`;
         const res = lo.dom(tag);
@@ -107,13 +18,7 @@ test('lo.dom() - create element', () => {
         assert.equal(res[0].textContent, '', msg);
     });
 
-    [
-        'body',
-        'frame',
-        'frameset',
-        'head',
-        'html'
-    ].forEach((name, idx) => {
+    'body frame frameset head html'.split(/\s+/).forEach((name, idx) => {
         const msg = `fix#${idx}: ${insp(name)} returns empty list`;
         const tag = `<${name}>`;
         const res = lo.dom(tag);
